6dcc00a29dcbbe46ea3998b7f21658a3d1425a2f
marulita18 <crespo.mariel@gmail.com>
2021-09-21 11:17:54 +0200

    redux setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { deposit } from "./store/balance/actions";
+import { selectBalance } from "./store/balance/selectors";
 
 function App() {
+  const dispatch = useDispatch();
+  const balance = useSelector(selectBalance);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Balance: {balance}$</p>
+      <button
+        onClick={() => {
+          dispatch(deposit(10));
+        }}
+      >
+        Deposit 10$
+      </button>
     </div>
   );
 }
